7e585e7b4cf588b784063f43183dcfab3cb3050b
Panos Kountanis <panosktn@gmail.com>
2017-04-27 13:33:59 +0300

    Introduce Nil Data Store class

---
+++ lib/Data/Store/Nil.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Class Mixtape_Data_Store_Nil
+ * Null object for datastores
+ */
+class Mixtape_Data_Store_Nil implements Mixtape_Interfaces_Data_Store {
+
+    /**
+     * @return Mixtape_Model_Collection
+     */
+    public function get_entities() {
+        return new Mixtape_Model_Collection( array() );
+    }
+
+    /**
+     * @param $id the id of the entity
+     * @return Mixtape_Model
+     */
+    public function get_entity( $id ) {
+        return Mixtape_Model::get_nil();
+    }
+
+    /**
+     * @param $entity Mixtape_Model
+     * @param $field_declaration Mixtape_Model_Field_Declaration
+     * @return mixed
+     */
+    public function get_meta_field_value($model, $field_declaration) {
+        return '';
+    }
+
+    /**
+     * @param $model Mixtape_Model
+     * @param array $args
+     * @return mixed
+     */
+    public function delete( $model, $args = array()) {
+        return true;
+    }
+
+    /**
+     * @param $entity Mixtape_Model
+     * @return mixed
+     */
+    public function upsert($entity, $fields, $meta_fields = array()) {
+        return 0;
+    }
+
+    public function is_nil() {
+        return true;
+    }
+}
