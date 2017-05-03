1812a97aea511f27eb9a6a9d26d8ed2c92f33c14
Panos Kountanis <panosktn@gmail.com>
2017-05-03 14:51:41 +0300

    add Declaraton_Test

---
+++ tests/unit/Mixtape/Model/Field/DeclarationTest.php
@@ -0,0 +1,63 @@
+<?php
+
+class Mixtape_Model_Field_DeclarationTest extends MixtapeModelTestCase {
+    function test_exists() {
+        $this->assertClassExists( 'Mixtape_Model_Field_Declaration' );
+    }
+
+    /**
+     * @expectedException Mixtape_Exception
+     */
+    function test_construct_throws_if_name_not_provided() {
+        new Mixtape_Model_Field_Declaration( array() );
+    }
+
+    /**
+     * @expectedException Mixtape_Exception
+     */
+    function test_construct_throws_if_type_not_provided() {
+        new Mixtape_Model_Field_Declaration( array('name' => 'foo') );
+    }
+
+    /**
+     * @expectedException Mixtape_Exception
+     */
+    function test_construct_throws_if_type_not_valid() {
+        new Mixtape_Model_Field_Declaration( array('name' => 'foo', 'type' => 'zap') );
+    }
+
+    function test_field_declarations() {
+
+        $sum_declaration = (new Mixtape_Model_Field_DeclarationBuilder())
+            ->of_type(Mixtape_Model_Field_Types::FIELD)
+            ->with_name( 'sum' )
+            ->with_value_type( Mixtape_Model_Field_Declaration::INT_VALUE )
+            ->build();
+        $this->assertTrue( $sum_declaration->is_field() );
+        $this->assertEquals( $sum_declaration->get_default_value(), 0 );
+        $this->assertEquals( $sum_declaration->get_name_to_map_from(), $sum_declaration->name );
+        $this->assertSame( $sum_declaration->cast_value('0'), 0);
+        $this->assertSame( $sum_declaration->cast_value( 0.1 ), 0);
+
+        $first_name_declaration = (new Mixtape_Model_Field_DeclarationBuilder())
+            ->of_type( Mixtape_Model_Field_Types::FIELD )
+            ->with_name( 'first_name' )
+            ->with_value_type( Mixtape_Model_Field_Declaration::STRING_VALUE )
+            ->with_default_value( 'Foobar' )
+            ->map_from( 'firstName' )
+            ->required(true)
+            ->build();
+        $this->assertTrue( $first_name_declaration->is_field() );
+        $this->assertTrue( $first_name_declaration->required );
+        $this->assertEquals( $first_name_declaration->get_default_value(), 'Foobar' );
+        $this->assertNotEquals( $first_name_declaration->get_name_to_map_from(), $first_name_declaration->name );
+        $this->assertSame($first_name_declaration->cast_value(0), '0');
+
+        $derived_declaration = (new Mixtape_Model_Field_DeclarationBuilder())
+            ->of_type( Mixtape_Model_Field_Types::DERIVED )
+            ->with_name( 'derived' )
+            ->build();
+        $this->assertTrue( $derived_declaration->is_derived_field() );
+        $this->assertEquals( $derived_declaration->get_default_value(), null );
+    }
+}
