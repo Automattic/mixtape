33aa63be4122e976f9673d2cbab00f96c3f5006c
Panos Kountanis <panosktn@gmail.com>
2017-05-31 10:58:03 +0300

    Refactor MT_Controller

--- lib/class-mt-controller.php
+++ lib/class-mt-controller.php
@@ -1,9 +1,17 @@
 <?php
+/**
+ * Controller
+ *
+ * @package Mixtape/Controller
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
-} // End if().
+}
 
+/**
+ * Class MT_Controller
+ */
 class MT_Controller extends WP_REST_Controller {
 	const HTTP_CREATED     = 201;
 	const HTTP_SUCCESS     = 200;
@@ -11,40 +19,55 @@ class MT_Controller extends WP_REST_Controller {
 	const HTTP_NOT_FOUND   = 404;
 
 	/**
-	 * @var MT_Controller_Bundle the bundle this belongs to
+	 * The bundle this belongs to.
+	 *
+	 * @var MT_Controller_Bundle
 	 */
 	protected $controller_bundle;
+
 	/**
-	 * @var string the endpoint base
+	 * The endpoint base (e.g. /users). Override in subclasses.
+	 *
+	 * @var string
 	 */
 	protected $base = null;
 
 	/**
-	 * @var null|MT_Environment optional, an enviromnent
+	 * Optional, an enviromnent
+	 *
+	 * @var null|MT_Environment
 	 */
 	protected $environment = null;
 
 	/**
 	 * MT_Rest_Api_Controller constructor.
 	 *
-	 * @param $controller_bundle MT_Controller_Bundle
-	 * @param null|MT_Environment                             $environment
-	 * @throws MT_Exception
+	 * @param MT_Controller_Bundle $controller_bundle The Controller Bundle.
+	 * @param null|MT_Environment  $environment The Environment.
+	 * @throws MT_Exception If no base is set.
 	 */
 	public function __construct( $controller_bundle = null, $environment = null ) {
+		MT_Expect::that( ! empty( $this->base ), 'Need to put a string with a backslash in $base' );
 		$this->controller_bundle = $controller_bundle;
-		if ( empty( $this->base ) ) {
-			throw new MT_Exception( 'Need to put a string with a backslash in $base' );
-		}
 		$this->set_environment( $environment );
 	}
 
+	/**
+	 * Set Controller Bundle
+	 *
+	 * @param MT_Controller_Bundle $controller_bundle Controller Bundle this belongs to.
+	 *
+	 * @return MT_Controller $this
+	 */
 	public function set_controller_bundle( $controller_bundle ) {
 		$this->controller_bundle = $controller_bundle;
+		return $this;
 	}
 
 	/**
-	 * @param MT_Environment|null $environment
+	 * Set the Environment for this Controller.
+	 *
+	 * @param MT_Environment|null $environment The Environment.
 	 * @return MT_Controller
 	 */
 	public function set_environment( $environment ) {
@@ -52,29 +75,73 @@ class MT_Controller extends WP_REST_Controller {
 		return $this;
 	}
 
+	/**
+	 * Registers this Controller.
+	 *
+	 * @throws MT_Exception Override this.
+	 */
 	public function register() {
 		throw new MT_Exception( 'override me' );
 	}
 
+	/**
+	 * Succeed
+	 *
+	 * @param array $data The dto.
+	 *
+	 * @return WP_REST_Response
+	 */
 	protected function succeed( $data ) {
-		return new WP_REST_Response( $data, self::HTTP_SUCCESS );
+		return $this->respond( $data, self::HTTP_SUCCESS );
 	}
 
+	/**
+	 * Created
+	 *
+	 * @param array $data The dto.
+	 *
+	 * @return WP_REST_Response
+	 */
 	protected function created( $data ) {
-		return new WP_REST_Response( $data, self::HTTP_CREATED );
+		return $this->respond( $data, self::HTTP_CREATED );
 	}
 
+	/**
+	 * Bad request
+	 *
+	 * @param array $data The dto.
+	 *
+	 * @return WP_REST_Response
+	 */
 	protected function fail_with( $data ) {
-		return new WP_REST_Response( $data, self::HTTP_BAD_REQUEST );
+		return $this->respond( $data, self::HTTP_BAD_REQUEST );
 	}
 
+	/**
+	 * Not Found (404)
+	 *
+	 * @param string $message The message.
+	 *
+	 * @return WP_REST_Response
+	 */
 	protected function not_found( $message ) {
-		return $this->respond( new WP_REST_Response( array(
+		return $this->respond( array(
 			'message' => $message,
-		), self::HTTP_NOT_FOUND ) );
+		), self::HTTP_NOT_FOUND );
 	}
 
-	public function respond( $thing ) {
-		return rest_ensure_response( $thing );
+	/**
+	 * Respond
+	 *
+	 * @param array|WP_REST_Response|WP_Error|mixed $data The thing.
+	 * @param int                                   $status The Status.
+	 *
+	 * @return mixed|WP_REST_Response
+	 */
+	private function respond( $data, $status ) {
+		if ( is_array( $data ) ) {
+			$data = new WP_REST_Response( $data, $status );
+		}
+		return rest_ensure_response( $data );
 	}
 }
